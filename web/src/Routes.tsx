a56e1fe9817c0ec08edcbc61508acf060d33bbf3
ecampos <ecampos.jr54@gmail.com>
2024-01-09 21:16:14 -0600

    add home and login pages

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,8 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/home" page={HomePage} name="home" />
+      <Route path="/login" page={LoginPage} name="login" />
       <Route path="/make-a-friend" page={MakeAFriendPage} name="makeAFriend" />
       <Route path="/ask-a-local" page={AskALocalPage} name="askALocal" />
       <Route path="/hotel" page={HotelPage} name="hotel" />
