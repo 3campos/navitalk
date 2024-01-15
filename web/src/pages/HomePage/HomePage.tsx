fed453caf7a9d642dfa163ef6424fdba30ccc1cd
ecampos <ecampos.jr54@gmail.com>
2024-01-15 00:18:17 -0600

    add responsive buttons w/links to all pages from home screen

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Button, ButtonGroup, Grid } from '@mui/material'
+
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
@@ -14,6 +16,43 @@ const HomePage = () => {
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
       </p>
+      <Grid container>
+        <Grid item xs={12} sm={4} md={4}>
+          <Link to={routes.personalBiography()}>
+            <Button variant="contained" sx={{ width: '10rem', margin: 1 }}>
+              Biography
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={4} md={4}>
+          <Link to={routes.restaurant()}>
+            <Button variant="contained" sx={{ width: '10rem', margin: 1 }}>
+              Restaurant
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={4} md={4}>
+          <Link to={routes.hotel()}>
+            <Button variant="contained" sx={{ width: '10rem', margin: 1 }}>
+              Hotel
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={4} md={4}>
+          <Link to={routes.askALocal()}>
+            <Button variant="contained" sx={{ width: '10rem', margin: 1 }}>
+              Ask A Local
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={4} md={4}>
+          <Link to={routes.makeAFriend()}>
+            <Button variant="contained" sx={{ width: '10rem', margin: 1 }}>
+              Make A Friend
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </>
   )
 }
