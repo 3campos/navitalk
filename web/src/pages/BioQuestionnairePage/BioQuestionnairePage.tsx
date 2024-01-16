2365abbd4f5200d3829d1b43813d338323f0b4dc
ecampos <ecampos.jr54@gmail.com>
2024-01-16 13:27:15 -0600

    add comment to BioQuestionnairePage to outline plan for this page

--- web/src/pages/BioQuestionnairePage/BioQuestionnairePage.tsx
+++ web/src/pages/BioQuestionnairePage/BioQuestionnairePage.tsx
@@ -1,6 +1,8 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
+// This page will prompt the user with a series of questions for topics that will likely arise during a conversation with a local. Questions will relate to the user's occupation (job title, what services their company provides, and their total time in the industry, etc.), the city/state/country they are from, foods that they enjoy, whether this is their first time visiting Japan, what the occasion is for their visit (vacation, business trip, honeymoon, etc.), what they do in their spare time.
+
 const BioQuestionnairePage = () => {
   return (
     <>
