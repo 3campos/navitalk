3b47faa93c47f1032002bed58b6f5f45ae65a47d
ecampos <ecampos.jr54@gmail.com>
2024-01-17 01:05:20 -0600

    adjust home button alignment on restaurant page

--- web/src/pages/RestaurantPage/RestaurantPage.tsx
+++ web/src/pages/RestaurantPage/RestaurantPage.tsx
@@ -1,4 +1,5 @@
-import { Button, ButtonGroup, Grid } from '@mui/material'
+import { Button, ButtonGroup, Box } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
 
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
@@ -9,11 +10,46 @@ const RestaurantPage = () => {
       <Metadata title="Restaurant" description="Restaurant page" />
       {console.log(routes.home().slice(1))}
       <h1>Restaurant</h1>
-      <Link to={routes.home()}>
-        <Button variant="contained" sx={{ width: '10rem', margin: 1 }}>
-          Home
-        </Button>
-      </Link>
+      <Grid container>
+        <Grid item xs={6} sm={6} md={6}>
+          <Link to={routes.personalBiography()}>
+            <Button variant="contained" sx={{ width: '8rem', margin: 1 }}>
+              Biography
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6}>
+          <Link to={routes.restaurant()}>
+            <Button variant="contained" sx={{ width: '8rem', margin: 1 }}>
+              Restaurant
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6}>
+          <Link to={routes.hotel()}>
+            <Button variant="contained" sx={{ width: '8rem', margin: 1 }}>
+              Hotel
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={6} md={6}>
+          <Link to={routes.hotel()}>
+            <Button variant="contained" sx={{ width: '8rem', margin: 1 }}>
+              Hotel
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+      <Box sx={{ mx: 'auto', width: 200 }}>
+        <Link to={routes.home()}>
+          <Button
+            variant="contained"
+            sx={{ justifyContent: 'center', width: '100%', mt: 1 }}
+          >
+            Home
+          </Button>
+        </Link>
+      </Box>
       {/* <p>
         My default route is named <code>restaurant</code>, link to me with `
         <Link to={routes.restaurant()}>Restaurant</Link>`
